59954ff178f7565a669c87a6bfb8e1370fa7d799
Tushar Dimble <dimbletushar7004@gmail.com>
2020-09-12 12:01:29 +0530

    sorting done

--- grid_02/assets/js/grid.js
+++ grid_02/assets/js/grid.js
@@ -1,6 +1,6 @@
 let orderData = [
     {
-        "order_id" : "101",
+        "order_id" : "109",
         "customer_name":"John Deo",
         "price":"674"
     },
@@ -36,8 +36,12 @@ let createUi = (data,index) => {
     // td 1
     let tbld = document.createElement('td');
     tblRow.appendChild(tbld);
+    // let orderSpan = document.createElement('span');
+    // orderSpan.innerHTML = data.order_id;
+    //tbld.appendChild(orderSpan);
+
     let orderIdInput = document.createElement("input");
-    setAttributes(orderIdInput,{'class':"test","onblur":'updateData('+ index +',this.value,\'order_id\')'});
+    setAttributes(orderIdInput,{'class':"test","onblur":'updateData('+ index + ',this.value,\'' + data.customer_name + '\',\'' + data.price + '\')'});
     orderIdInput.value = data.order_id;
     tbld.appendChild(orderIdInput);
 
@@ -45,7 +49,7 @@ let createUi = (data,index) => {
     let tbld1 = document.createElement('td');
     tblRow.appendChild(tbld1);
     let custNameInput = document.createElement("input");
-    setAttributes(custNameInput,{'class':"test","onblur":'updateData('+ index +',this.value,\'customer_name\')'});
+    setAttributes(custNameInput,{'class':"test","onblur":'updateData('+ index + ',\''+ data.order_id +'\',this.value,\''+data.price+'\')'});
     custNameInput.value = data.customer_name;
     tbld1.appendChild(custNameInput);
 
@@ -53,12 +57,10 @@ let createUi = (data,index) => {
     let tbld2 = document.createElement('td');
     tblRow.appendChild(tbld2);
     let priceInput = document.createElement("input");
-    setAttributes(priceInput,{'class':"test","onblur":'updateData('+ index +',this.value,\'price\')'});
+    setAttributes(priceInput,{'class':"test","onblur":'updateData('+ index + ',\''+ data.order_id +'\',\''+data.customer_name+'\',this.value)'});
     priceInput.value = data.price;
     tbld2.appendChild(priceInput);
 }
-
-
 
 let displayGrid = () => {
     orderData.map(function(data,index){
@@ -66,13 +68,58 @@ let displayGrid = () => {
     });
 }
 
-
-
 displayGrid();
 
-let updateData = (index,value,key) => {
-    orderData[index][key] = value;
+let updateData = (index,order_id,customer_name,price) => {
+    // orderData[index][key] = value;
+    // console.log(orderData);
+    let updatedObj = {
+        "order_id" :order_id,
+        "customer_name":customer_name,
+        "price":price
+    }
+    Object.assign(orderData[index],updatedObj);
     console.log(orderData);
+
+    document.getElementById('tbody').innerHTML = "";
+    displayGrid();
 }
+
+let sort = (property,order) => {
+    let sort_order = 1;
+    if(order === "desc"){
+        sort_order = -1;
+    }
+    return function (a, b){
+        if(a[property] < b[property]){
+            return -1 * sort_order;
+        }else if(a[property] > b[property]){
+            return 1 * sort_order;
+        }else{
+            return 0 * sort_order;
+        }
+    }
+}
+
+let sort1 = (property,order) => {
+    let newOrder;
+    if(order === "asc"){
+        newOrder = "desc";
+        document.getElementById(property).classList.remove("asc");
+        document.getElementById(property).classList.add("desc");
+    }else{
+        newOrder = "asc";
+        document.getElementById(property).classList.remove("desc");
+        document.getElementById(property).classList.add("asc");
+    }
+    document.getElementById(property).setAttribute('onclick','sort1(\''+property+'\',\''+newOrder+'\')');
+    orderData.sort(sort(property,order));
+
+    document.getElementById('tbody').innerHTML = "";
+    displayGrid();
+
+}
+
+
 
 
